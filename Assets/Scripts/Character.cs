92a33d0d4d807ddb18be95feba18936a99139583
Yves Yuen <yves.j.yuen@gmail.com>
2018-05-11 04:36:37 -0700

    add  movement logic

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,14 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-	public int offsetY = 5;
 	// Use this for initialization
+	public float speed = 0.5f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+	public Transform path;
+	public Vector3 direction;
+	public GameObject currentTile;
+
+	public float destinationX;
+	public float destinationY;
+	
+	public bool hasReachedDestination;
+
+	public GameObject map;
+	public string directionString = "IDLE";
+
+	Vector3 UP = new Vector3(0, 1, 0);
+	Vector3 DOWN = new Vector3(0, -1, 0);
+	Vector3 LEFT = new Vector3(-1, 0, 0);
+	Vector3 RIGHT = new Vector3(1, 0, 0);
+	Vector3 IDLE = new Vector3(0, 0, 0);
 	void Start () {
-		
+		// direction = UP;
+		map = GameObject.Find("MapManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		move();
 	}
+
+	void move() {
+		hasReachedDestination = checkPassedDestination();
+		if (!hasReachedDestination) {
+			transform.position += direction * Time.deltaTime * speed; 
+		}
+	}
+
+	bool checkPassedDestination() {
+		switch(directionString) {
+			case "UP":
+				Debug.Log(destinationY);
+				return transform.position.y > destinationY;
+			case "DOWN":
+				return transform.position.y < destinationY;
+			case "LEFT":
+				return transform.position.x < destinationX;
+			case "RIGHT":
+				return transform.position.x > destinationX;
+		}
+		return true;
+	}
+	// public void setDirection(string dirStr) {
+	// 	directionString = dirStr;
+	// 	switch(dirStr) {
+	// 		case "UP":
+	// 			direction = UP;
+	// 			break;
+	// 		case "DOWN":
+	// 			direction = DOWN;
+	// 			break;
+	// 		case "LEFT":
+	// 			direction = LEFT;
+	// 			break;
+	// 		case "RIGHT":
+	// 			direction = RIGHT;
+	// 			break;
+	// 	}
+	// }
+
+	public void setDestination(float x, float y) {
+		destinationX = x;
+		destinationY = y;
+		if (transform.position.x > x) {
+			directionString = "LEFT";
+			direction = LEFT;
+		} else if (transform.position.x < x) {
+			directionString = "RIGHT";
+			direction = RIGHT;
+		} else if (transform.position.y < y) {
+			directionString = "UP";
+			direction = UP;
+		} else if (transform.position.y > y) {
+			directionString = "DOWN";
+			direction = DOWN;
+		} else {
+			directionString = "IDLE";
+			direction = IDLE;
+		}
+		Debug.Log("GOING UP");
+	}
+
+	// public void setDestination(GameObject tile) {
+	// 	destination = tile;
+	// 	if (transform.position.x > tile.transform.position.x) {
+	// 		direction = LEFT;
+	// 	} else if (transform.position.x < tile.transform.position.x) {
+	// 		direction = RIGHT;
+	// 	} else if (transform.position.y < tile.transform.position.y) {
+	// 		direction = UP;
+	// 	} else if (transform.position.y > tile.transform.position.y) {
+	// 		direction = DOWN;
+	// 	} else {
+	// 		direction = IDLE;
+	// 	}
+
+	// }
 }
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -5,51 +5,78 @@ using UnityEngine;
 public class MapManager : MonoBehaviour {
 	// public GameObject[] prefabs = new GameObject[1];
 	public List<GameObject> prefabList = new List<GameObject>();
-
+	public GameObject[,] map;
 	public GameObject character;
 	public int width;
 	public int height;
 
+	private float startingX = 0f;
+	private float startingY = 0f;
+
+	public GameObject nextTile;
+
 	void Start(){
-		// int prefabIndex = UnityEngine.Random.Range(0,prefabList.Count-1);
 		createMap();
 		shuffleMap();
 		spawnCharacter();
     }
 
+	void Update () {
+		if (character.GetComponent<Character>().hasReachedDestination) {
+			Debug.Log("reached destination!");
+			
+		}
+	}
+
 	void createMap() {
+		map = new GameObject[width, height];
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				Vector2 position = new Vector2(x,y);
 				int prefabIndex;
-				if (Random.Range(0, 100) > 90) {
+				if (x == 0 && y == 0) {
+					// populate a line n 0,0
+					prefabIndex = 0;
+				}
+				else if (Random.Range(0, 100) > 90) {
 					prefabIndex = prefabList.Count - 1;
 				} else {
 					prefabIndex = Random.Range(0, prefabList.Count-1);
 				}
 				GameObject tile = Instantiate(prefabList[prefabIndex]);
+				map[x,y] = tile;
 				tile.transform.position = position;
 			}
 		}
 	}
 
 	void shuffleMap() {
-		foreach (var currentTile in GameObject.FindGameObjectsWithTag("Tile")) {
-			int rand = Random.Range(0,4);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				int rand;
+				if(x == 0 && y ==0) {
+					rand = 0;
+				} else {
+					rand = Random.Range(0,4);
+				}
 
-			for (int i = 0; i < rand; i++) {
-				currentTile.GetComponent<Tile>().rotateTile();
+				for (int i = 0; i < rand; i++) {
+					map[x,y].GetComponent<Tile>().rotateTile();
+				}
+
 			}
 		}
 	}
 
-	void spawnCharacter() {
-		Vector3 position = new Vector3(0, 0, -1);
-		GameObject tile = Instantiate(character);
-		tile.transform.position = position;
+	public GameObject getTileAt(int x,int y) {
+		return map[x,y];
 	}
 
-	void Update () {
-		
+	void spawnCharacter() {
+		Vector3 position = new Vector3(0, -0.5f, -1);
+		character = Instantiate(character);
+		character.transform.position = position;
+		character.GetComponent<Character>().setDestination(startingX, startingY);
+		nextTile = getTileAt(0,0);
 	}
 }
