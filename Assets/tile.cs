57c29b146ce4c452bd8e2f12aa01c6fec0d7a548
Yves Yuen <yves.j.yuen@gmail.com>
2018-05-10 21:34:21 -0700

    initial commit

---
+++ Assets/tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile : MonoBehaviour {
+	float rotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onMouseDown() {
+		rotation += 90;
+
+		transform.rotation = Quaternion.Euler(0, 0, rotation);
+	}
+}
