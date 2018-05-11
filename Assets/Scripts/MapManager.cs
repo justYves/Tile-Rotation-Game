9c85e15d413ee24d07be64aa588e89d718198916
Yves Yuen <yves.j.yuen@gmail.com>
2018-05-11 00:23:56 -0700

    map generation working

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+	// public GameObject[] prefabs = new GameObject[1];
+	public List<GameObject> prefabList = new List<GameObject>();
+	public int width;
+	public int height;
+
+	// public Map map;
+	// Use this for initialization
+	// void Start () {
+		// Vector2 mapSize = getMapSize();
+
+		// map.width = (int) mapSize.x;
+		// map.height = (int) mapSize.y;
+
+		// map.tiles = new tile[map.width, map.height];
+
+		// GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+		// foreach (var tile in tiles) {
+		// 	map.tiles[(int) tile.transform.position.x, (int) tile.transform.position.y] = tile.GetComponent<tile>();
+		// }
+		// foreach (var tile in map.tiles) {
+		// 	Debug.Log(tile.gameObject.name);
+		// }
+	// }
+
+	void Start(){
+		// int prefabIndex = UnityEngine.Random.Range(0,prefabList.Count-1);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				Vector2 position = new Vector2(x,y);
+				GameObject tile = Instantiate(prefabList[Random.Range(0, prefabList.Count)]);
+				tile.transform.position = position;
+			}
+		}
+    }
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/tile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile : MonoBehaviour {
+	float rotation;
+	float rotationIncrement = 90;
+	public int[] roadValues;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.rotation.eulerAngles.z != rotation) {
+			transform.rotation = Quaternion.Lerp(
+				transform.rotation,
+				Quaternion.Euler(0, 0, rotation),
+				speed
+			);
+		}
+	}
+
+	void OnMouseDown() {
+		rotation += rotationIncrement;
+		rotation = rotation == 360 ? 0 : rotation;
+	}
+
+	public void rotateTile() {
+		transform.rotation = Quaternion.Euler(0, 0, rotation);
+		updateValues();
+	}
+
+	public void updateValues() {
+		int last = roadValues[0];
+		int i = 0;
+		while(i < 3) {
+			roadValues[i] = roadValues[++i];
+		}
+
+		roadValues[3] = last;
+	}
+}
